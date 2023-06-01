b21f54cbc7cd00dbca0188821ed38acb503e16f1
lucky-tensor <77554843+lucky-tensor@users.noreply.github.com>
2023-06-01 17:00:44 -0400

    wip process snapshot

--- tools/genesis/src/main.rs
+++ tools/genesis/src/main.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
-use libra_genesis_tools::wizard::GenesisWizard;
-
+use libra_genesis_tools::{wizard::GenesisWizard, parse_json};
+use ol_genesis_tools::process_snaphot;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct GenesisCliArgs {
@@ -13,10 +13,13 @@ struct GenesisCliArgs {
 #[derive(Subcommand)]
 enum Sub {
     /// does testing things
-    Fork {
+    ExportDb {
         /// lists test values
         #[arg(short, long)]
-        test_mode: bool,
+        snapshot_path: PathBuf,
+
+        #[arg(short, long)]
+        output_path: Option<PathBuf>,
     },
     Wizard {
         /// choose a different home data folder for all node data.
@@ -33,9 +36,10 @@ enum Sub {
 fn main() -> anyhow::Result<()>{
     let cli = GenesisCliArgs::parse();
     match cli.command {
-        Some(Sub::Fork { test_mode }) => {
-            dbg!(&test_mode);
-            // make_recovery_genesis_from_vec_legacy_recovery();
+        Some(Sub::ExportDb { snapshot_path , output_path}) => {
+          let lr = process_snaphot::db_backup_into_recovery_struct(snapshot_path);
+          lr.write_recovery_file(output_path);
+
         }
         Some(Sub::Wizard { home_dir, local_framework }) => {
             GenesisWizard::default().start_wizard(home_dir, local_framework)?;
